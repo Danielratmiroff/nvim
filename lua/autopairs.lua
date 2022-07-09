e6a0bfc0eaf88d23d25ffeb2da6233e6947e4d83
Daniel Ratmiroff <danielratmiroff@gmail.com>
2022-07-09 19:55:22 +0200

    telescope my ass

--- lua/autopairs.lua
+++ lua/autopairs.lua
@@ -1,8 +1,3 @@
-local status_ok, nvim-autopairs = pcall(require, "nvim-autopairs")
-if not status_ok then
-  return "Something failed with Autopairs"
-end
-
 local npairs = require('nvim-autopairs')
 
 npairs.setup({fast_wrap = {
--- lua/telescope.lua
+++ lua/telescope.lua
@@ -3,32 +3,18 @@ if not status_ok then
   return "Something failed with Telescope"
 end
 
+-- local actions = require "telescope.actions" Need to define new keymaps for navigation
+
 telescope.setup({
   defaults = {
       file_ignore_patterns = {
         "node_modules/.*",
         "secret.d/.*",
-        "%.pem"
-      }
+        "%.pem",
+        ".git/",
 
+      },
+       path_display = { "smart" },
     }
   })
 
-  local map = vim.api.nvim_set_keymap
-
-  local options = { noremap = true }
-
-  -- Builtin
-  map('n', '<leader>fg', '<CMD>lua require("telescope.builtin").git_files{}<CR>', options)
-  map('n', '<leader>ff', '<CMD>lua require("telescope.builtin").find_files{ hidden = true }<CR>', options)
-  map('n', '<leader>fl', '<CMD>lua require("telescope.builtin").live_grep()<CR>', options)
-  map('n', '<leader>fb', '<CMD>lua require("telescope.builtin").buffers()<CR>', options)
-  map('n', '<leader>fh', '<CMD>lua require("telescope.builtin").help_tags()<CR>', options)
-  map('n', '<leader>fd', '<CMD>lua require("telescope.builtin").diagnostics()<CR>', options)
-  map('n', '<leader>fr', '<CMD>lua require("telescope.builtin").registers()<CR>', options)
-
-  -- Language Servers
-  map('n', '<leader>lsd', '<CMD>lua require("telescope.builtin").lsp_definitions{}<CR>', options)
-  map('n', '<leader>lsi', '<CMD>lua require("telescope.builtin").lsp_implementations{}<CR>', options)
-  map('n', '<leader>lsl', '<CMD>lua require("telescope.builtin").lsp_code_actions{}<CR>', options)
-  map('n', '<leader>lst', '<CMD>lua require("telescope.builtin").lsp_type_definitions{}<CR>', options)

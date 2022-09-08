fb32de6a36d0c0f58eb1d04ba5628ea3ccaaa20c
dan <danielratmiroff@gmail.com>
2022-09-08 09:20:49 +0200

    update to tokyo darker

--- lua/_colorscheme.lua
+++ lua/_colorscheme.lua
@@ -1,4 +1,4 @@
--- Color scheme 
+-- Color scheme
 
 vim.cmd([[ 
 if has('termguicolors')
@@ -6,16 +6,16 @@ set termguicolors
 endif
 ]])
 
-  -- Theme settings
-  vim.g.tokyonight_style = "night"
-  vim.g.tokyonight_italic_functions = true
-  vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-  vim.g.tokyonight_transparent = true
+-- Theme settings
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+vim.g.tokyonight_transparent = true
 
-  -- Load colorscheme
-  vim.cmd[[colorscheme tokyonight]]
+-- Load colorscheme
+vim.cmd [[colorscheme tokyonight-night]]
 
-  -- Edge colorscheme
+-- Edge colorscheme
 --let g:edge_style = 'aura'
 --let g:edge_better_performance = 1
 --colorscheme edge
